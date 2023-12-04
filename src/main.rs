9265113f8cf23de24be49a11ed83bc34fc57e1b8
AshGw <AshrefGw@proton.me>
2023-12-04 22:45:46 +0100

    some good ol' refactoring

--- src/main.rs
+++ src/main.rs
@@ -8,51 +8,29 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
-        eprintln!("Usage: {} <logo> <image> [debug]", args[0]);
+        eprintln!("Usage: {} <logo> <image> [debug] [output_path]", args[0]);
         std::process::exit(1);
     }
 
     let logo_path: &String = &args[1];
     let main_path: &String = &args[2];
-    let debug: bool = args.len() > 3 && args[3] == "debug";
+    let debug: bool = args.contains(&String::from("debug"));
+
+    let output_path: &str = args
+        .iter()
+        .position(|arg: &String| arg == "debug")
+        .map_or("./result_of_both.jpg", |index: usize| args.get(index + 1)
+        .map_or("./result_of_both.jpg", String::as_str));
 
     if debug {
         println!("Debug mode enabled.");
     }
 
-    let logo_image: image::DynamicImage = ImageReader::open(logo_path)
-        .unwrap_or_else(|e| {
-            if debug {
-                eprintln!("Failed to open the logo image: {}", e);
-            }
-            std::process::exit(1);
-        })
-        .decode()
-        .unwrap_or_else(|e| {
-            if debug {
-                eprintln!("Failed to decode the logo image: {}", e);
-            }
-            std::process::exit(1);
-        });
-
-    let main_image: image::DynamicImage = ImageReader::open(main_path)
-        .unwrap_or_else(|e| {
-            if debug {
-                eprintln!("Failed to open the main image: {}", e);
-            }
-            std::process::exit(1);
-        })
-        .decode()
-        .unwrap_or_else(|e| {
-            if debug {
-                eprintln!("Failed to decode the main image: {}", e);
-            }
-            std::process::exit(1);
-        });
+    let logo_image: image::DynamicImage = open_and_decode_image(logo_path, "logo", debug);
+    let main_image: image::DynamicImage = open_and_decode_image(main_path, "main", debug);
 
     let result_image: image::DynamicImage = overlay_images(logo_image, main_image);
 
-    let output_path: &str = "./result_of_both.jpg";
     result_image.save(output_path).unwrap_or_else(|e| {
         if debug {
             eprintln!("Failed to save result image: {}", e);
@@ -63,4 +41,21 @@ fn main() {
     if debug {
         println!("Result image saved to {}", output_path);
     }
+}
+
+fn open_and_decode_image(path: &str, image_type: &str, debug: bool) -> image::DynamicImage {
+    ImageReader::open(path)
+        .unwrap_or_else(|e: std::io::Error| {
+            if debug {
+                eprintln!("Failed to open the {} image: {}", image_type, e);
+            }
+            std::process::exit(1);
+        })
+        .decode()
+        .unwrap_or_else(|e: image::ImageError| {
+            if debug {
+                eprintln!("Failed to decode the {} image: {}", image_type, e);
+            }
+            std::process::exit(1);
+        })
 }
