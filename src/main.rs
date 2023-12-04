030f446826515dd8a9fde1a0a70ff65ffc5d1b76
AshGw <AshrefGw@proton.me>
2023-12-04 22:10:32 +0100

    basic usage set

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod processor;
+
+use std::env;
+use image::io::Reader as ImageReader;
+use crate::processor::processor::overlay_images;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <logo_path> <main_path>", args[0]);
+        std::process::exit(1);
+    }
+
+    let logo_path = &args[1];
+    let main_path = &args[2];
+
+    let logo_image = ImageReader::open(logo_path)
+        .expect("Failed to open logo image")
+        .decode()
+        .unwrap();
+
+    let main_image = ImageReader::open(main_path)
+        .expect("Failed to open main image")
+        .decode()
+        .unwrap();
+
+    let result_image = overlay_images(logo_image, main_image);
+
+    let output_path = "./result_of_both.jpg";
+    result_image.save(output_path).expect("Failed to save result image");
 }
---
+++ src/processor/processor.rs
@@ -0,0 +1,23 @@
+use image::{DynamicImage, imageops};
+
+pub fn overlay_images(logo_image: DynamicImage, main_image: DynamicImage) -> DynamicImage {
+    let mut main_rgba = main_image.to_rgba8();
+
+    let (main_width, main_height) = main_rgba.dimensions();
+    let logo_width = 830;
+    let logo_height = 142;
+    let margin = 10;
+    let logo_position_x = (main_width - logo_width - margin) as i64;
+    let logo_position_y = (main_height - logo_height - margin) as i64;
+
+    let resized_logo = logo_image.resize_exact(
+        logo_width,
+        logo_height,
+        imageops::FilterType::Lanczos3,
+    ); 
+    let logo_rgba = resized_logo.to_rgba8();
+
+    imageops::overlay(&mut main_rgba, &logo_rgba, logo_position_x, logo_position_y);
+
+    DynamicImage::ImageRgba8(main_rgba)
+}
