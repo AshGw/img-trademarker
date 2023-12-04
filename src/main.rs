9abb11007e99f8b32f199765eed8f49505b5c571
AshGw <AshrefGw@proton.me>
2023-12-04 22:12:15 +0100

    added extra types

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,21 @@ fn main() {
         std::process::exit(1);
     }
 
-    let logo_path = &args[1];
-    let main_path = &args[2];
+    let logo_path: &String = &args[1];
+    let main_path: &String = &args[2];
 
-    let logo_image = ImageReader::open(logo_path)
+    let logo_image: image::DynamicImage = ImageReader::open(logo_path)
         .expect("Failed to open logo image")
         .decode()
         .unwrap();
 
-    let main_image = ImageReader::open(main_path)
+    let main_image: image::DynamicImage = ImageReader::open(main_path)
         .expect("Failed to open main image")
         .decode()
         .unwrap();
 
-    let result_image = overlay_images(logo_image, main_image);
+    let result_image: image::DynamicImage = overlay_images(logo_image, main_image);
 
-    let output_path = "./result_of_both.jpg";
+    let output_path: &str = "./result_of_both.jpg";
     result_image.save(output_path).expect("Failed to save result image");
 }
--- src/processor/processor.rs
+++ src/processor/processor.rs
@@ -1,21 +1,21 @@
 use image::{DynamicImage, imageops};
 
 pub fn overlay_images(logo_image: DynamicImage, main_image: DynamicImage) -> DynamicImage {
-    let mut main_rgba = main_image.to_rgba8();
+    let mut main_rgba: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> = main_image.to_rgba8();
 
     let (main_width, main_height) = main_rgba.dimensions();
-    let logo_width = 830;
-    let logo_height = 142;
-    let margin = 10;
-    let logo_position_x = (main_width - logo_width - margin) as i64;
-    let logo_position_y = (main_height - logo_height - margin) as i64;
+    let logo_width: u32 = 830;
+    let logo_height: u32 = 142;
+    let margin: u32 = 10;
+    let logo_position_x: i64 = (main_width - logo_width - margin) as i64;
+    let logo_position_y: i64 = (main_height - logo_height - margin) as i64;
 
-    let resized_logo = logo_image.resize_exact(
+    let resized_logo: DynamicImage = logo_image.resize_exact(
         logo_width,
         logo_height,
         imageops::FilterType::Lanczos3,
     ); 
-    let logo_rgba = resized_logo.to_rgba8();
+    let logo_rgba: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> = resized_logo.to_rgba8();
 
     imageops::overlay(&mut main_rgba, &logo_rgba, logo_position_x, logo_position_y);
 
